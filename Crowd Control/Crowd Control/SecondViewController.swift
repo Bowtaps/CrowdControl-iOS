c500151d74c0300253e7ee50fb615f8449146481
Deaboy <danieleandrus@gmail.com>
2015-10-15 17:00:40 -0600

    Added project files
    + Added template project files
    + Added .gitignore

---
+++ Crowd Control/Crowd Control/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Crowd Control
+//
+//  Created by Daniel Andrus on 2015-10-15.
+//  Copyright © 2015 Bowtaps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
