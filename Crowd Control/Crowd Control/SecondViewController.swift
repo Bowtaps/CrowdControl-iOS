3d95c3439abae1599a0a622c4462c61b0eaaa1b3
Deaboy <danieleandrus@gmail.com>
2015-10-22 00:17:54 -0600

    Added map to tab storyboard
    + Added MapKit to project
    + Added map view to tabbed storyboard

--- Crowd Control/Crowd Control/SecondViewController.swift
+++ Crowd Control/Crowd Control/SecondViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
 class SecondViewController: UIViewController {
 
+	@IBOutlet weak var mNavBar: UINavigationBar!
+	@IBOutlet weak var mMap: MKMapView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		let userTrackingButton = MKUserTrackingBarButtonItem(mapView: mMap)
 	}
 
 	override func didReceiveMemoryWarning() {
