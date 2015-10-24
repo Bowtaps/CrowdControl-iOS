550cb40de6903308e3d1265754106a9ee7e965a0
Hamm3rTim3 <evan.hammer@mines.sdsmt.edu>
2015-10-24 15:34:04 -0600

    Added the getters and setters to the CCUserModel class

--- Crowd Control/Crowd Control/CCUserModel.swift
+++ Crowd Control/Crowd Control/CCUserModel.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+class CCUserModel {
+    var status: String
+    var location: (Double, Double)
+    var preferences: String
+    var thing: String
+    
+    init(status: String, location: (Double, Double), preferences: String, thing: String){
+        self.status = status
+        self.location = location
+        self.preferences = preferences
+        self.thing = thing
+        
+    }
+}
+
+extension CCUserModel {
+    func getCCUser() -> CCUserModel{
+        return self
+    }
+    func getUsername() -> String {
+        //Do get username here
+        return "TODO"
+    }
+    func getLocation() -> (Double, Double) {
+        return self.location
+    }
+    func getPreferences() -> String {
+        return self.preferences
+    }
+    func getStatus() -> String {
+        return self.status
+    }
+    func setLocation(long: Double, lat: Double){
+        self.location = (long, lat)
+    }
+    func setPreferences(pref: String){
+        self.preferences = pref
+    }
+    func setStatus(status: String){
+        self.status = status
+    }
+}
