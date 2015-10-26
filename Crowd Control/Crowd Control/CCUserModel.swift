fcd4d6b7cb99552e65dda66b4ec73a682a6ed1ba
Hamm3rTim3 <evan.hammer@mines.sdsmt.edu>
2015-10-26 09:40:28 -0600

    Added the variables in the Parse User table

--- Crowd Control/Crowd Control/CCUserModel.swift
+++ Crowd Control/Crowd Control/CCUserModel.swift
@@ -9,36 +9,72 @@
 import Foundation
 
 class CCUserModel {
+    //CCUser Table in Parse
     var status: String
     var location: (Double, Double)
     var preferences: String
-    var thing: String
     
-    init(status: String, location: (Double, Double), preferences: String, thing: String){
+    //ParseUser Table in Parse
+    var objectId: String
+    var username: String
+    var password: String
+    var authData: String
+    var emailVerified: Bool
+    var email: String
+    var createdAt: NSDate
+    var updatedAt: NSDate
+    
+    init(status: String, location: (Double, Double), preferences: String, objectId: String, username: String, password: String, authData: String, emailVerified: Bool, email: String, createdAt: NSDate, updatedAt: NSDate){
         self.status = status
         self.location = location
         self.preferences = preferences
-        self.thing = thing
-        
+        self.objectId = objectId
+        self.username = username
+        self.password = password
+        self.authData = authData
+        self.emailVerified = emailVerified
+        self.email = email
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
     }
 }
 
 extension CCUserModel {
+    func getAuthData() -> String{
+        return self.authData
+    }
     func getCCUser() -> CCUserModel{
         return self
     }
-    func getUsername() -> String {
-        //Do get username here
-        return "TODO"
+    func getCreatedAt() -> NSDate{
+        return self.createdAt
+    }
+    func getEmail() -> String{
+        return self.email
+    }
+    func getEmailVerified() -> Bool{
+        return self.emailVerified
     }
     func getLocation() -> (Double, Double) {
         return self.location
+    }
+    func getObjectId() -> String{
+        return self.objectId
+    }
+    func getPassword() -> String{
+        return self.password
     }
     func getPreferences() -> String {
         return self.preferences
     }
     func getStatus() -> String {
         return self.status
+    }
+    func getUpdatedAt() -> NSDate{
+        return self.updatedAt
+    }
+    func getUsername() -> String {
+        return self.username
     }
     func setLocation(long: Double, lat: Double){
         //save location to parse
