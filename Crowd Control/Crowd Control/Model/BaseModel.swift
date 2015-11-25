5bff2d5ca1cd8e05f3a383e73cf5a8bade0fc79c
Deaboy <danieleandrus@gmail.com>
2015-11-24 22:08:15 -0700

    Added more specific protocols. Added a more clear inheritance hierarchy.
    + Added protocol BaseModel and documentation
    + Added protocol UserProfileModel and documentation
    * Moved many fields from UserModel to BaseModel

---
+++ Crowd Control/Crowd Control/Model/BaseModel.swift
@@ -0,0 +1,81 @@
+//
+//  BaseModel.swift
+//  Crowd Control
+//
+//  Created by Daniel Andrus on 2015-11-24.
+//  Copyright © 2015 Bowtaps. All rights reserved.
+//
+
+import Foundation
+
+/// Base model protocol, which is to be implemented by all model classes in the project. Provides
+/// access to basic information about the model, such as:
+///
+/// - Unique identifier
+/// - Creation timestamp
+/// - Last updated timestamp
+/// - Flag indicating modification since last push/pull
+protocol BaseModel {
+	
+	/// The ID of the object as determined by remote storage. This value is automatically generated
+	/// when the object is stored and is usually determined by a remote server. It is valid for this
+	/// value to be nil, and thus should be used with care and only if absolutely necessary.
+	var id: String? {get}
+	
+	/// Timestamp of when this object was first created and stored. This value may be automatically
+	/// determined by the server if the model is using a remote machine for storage.
+	var created: NSDate? {get}
+	
+	/// Timestamp of the last time this object was updated in storage. This value may be
+	/// automatically determined by the server if the model is using a remote machine for storage.
+	var update: NSDate? {get}
+	
+	/// Flag indicating whether or not this object has been modified since it was last pulled from
+	/// or pushed to storage.
+	var modified: Bool {get}
+	
+	/// Loads this object from storage, either local or remote as determined by the implementation.
+	///
+	/// This is a blocking function and should be executed on a thread separate from the main
+	/// thread. See `loadInBackground()` for loading this object on a separate thread. This function
+	/// will throw an exception if an error occurs.
+	///
+	/// - SeeAlso: `loadInBackground(_:)`
+	func load()
+	
+	/// Loads this object from storage, either local or remote as determined by the implementation.
+	///
+	/// This function spawns a new thread and reloads this object from storage. After successful
+	/// execution or if an error occurs, this function passes control back to the main thread by
+	/// calling the closure that was optionally passed as an argument.
+	///
+	/// - Parameter callback: Optional callback function to call after successful execution or if an
+	///                       error occurs. If `nil` is provided, then the callback will not be
+	///                       called.
+	///
+	/// - SeeAlso: `load()`
+	func loadInBackground(callback: ((object: BaseModel?, error: NSError?) -> Void)?)
+	
+	/// Saves this object storage, either local or remote as determined by the implementation.
+	///
+	/// This is a blocking function and should be executed on a thread separate from the main
+	/// thread. See `saveInBackground()` for saving this object on a separate thread. This function
+	/// will throw an exception if an error occurs.
+	///
+	/// - SeeAlso: `saveInBackground(_:)`
+	func save()
+	
+	/// Saves this object to storage, either local or remote as determined by the implementation.
+	///
+	/// This function spawns a new thread and sends this object to storage. After successful
+	/// execution or if an error occurs, this function passes control back to the main thread by
+	/// calling the closure that was optionally passed as an argument.
+	///
+	/// - Parameter callback: Optional callback function to call after successful execution or if an
+	///                       error occurs. If `nil` is provided, then the callback will not be
+	///                       called.
+	///
+	/// - SeeAlso: `save()`
+	func saveInBackground(callback: ((object: BaseModel?, error: NSError?) -> Void)?)
+	
+}
--- Crowd Control/Crowd Control/Model/UserProfileModel.swift
+++ Crowd Control/Crowd Control/Model/UserProfileModel.swift
@@ -1,16 +1,24 @@
 //
-//  UserProfile.swift
+//  UserProfileModel.swift
 //  Crowd Control
 //
-//  Created by Evan Hammer on 11/24/15.
+//  Created by Daniel Andrus on 2015-11-24.
 //  Copyright © 2015 Bowtaps. All rights reserved.
 //
 
 import Foundation
 
-protocol UserProfileModel {
-    var displayName: String {get set}
-    var location: (Double, Double) {get set}
-    var preferences: String? {get set}
-    
+/// Model protocol representing the public profile of any user in the app. This model is intended to
+/// be an adequate interface for interacting with other users of the app, as it contains sufficient
+/// information about the other user to communicate with them.
+protocol UserProfileModel: BaseModel {
+	
+	/// The outward-facing display name of the other user. May be `nil` if this value has not been
+	/// set. This value is not unique to users an should *never* be used as an index or identifier.
+	/// This value can and should, however, be used in the following capacities:
+	///
+	/// - Displayed as a label identifying the user to other users
+	/// - Returned as a search result when and if searching for users is ever performed
+	var displayName: String? {get set}
+	
 }
