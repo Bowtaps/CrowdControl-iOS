df254d6716790dde6bf00a011696784a1806bb79
Hamm3rTim3 <evan.hammer@mines.sdsmt.edu>
2015-12-30 12:30:44 -0600

    Now the file should be commited

---
+++ Crowd Control/Crowd Control/Model/LocationModel.swift
@@ -0,0 +1,26 @@
+//
+//  LocationModel.swift
+//  Crowd Control
+//
+//  Created by Evan Hammer on 12/29/15.
+//  Copyright © 2015 Bowtaps. All rights reserved.
+//
+
+import Foundation
+
+/// Model protocol representing a location in string form also a sender and a recipient
+protocol LocationModel: BaseModel{
+    /// String holding the longitudinal coordinate
+    var longitude: String {get set}
+    
+    /// String holding the latitudinal coordinate
+    var latitude: String {get set}
+    
+    /// UserProfileModel holding the recipient information when the locations are being sent
+    /// to parse so that they can be sent encrypted to the intended recipient
+    var recipient: UserProfileModel {get}
+    
+    /// This is the Current users' UserProfileModel holding the reference for the storage on 
+    /// parse.
+    var sender: UserProfileModel {get}
+}
